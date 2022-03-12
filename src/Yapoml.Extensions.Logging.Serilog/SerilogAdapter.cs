6d18913784f12a5dc2ca914a13f7c5548f12bec3
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2022-03-12 21:42:03 +0300

    Added source for serilog

---
+++ src/Yapoml.Extensions.Logging.Serilog/SerilogAdapter.cs
@@ -0,0 +1,19 @@
+﻿using Serilog;
+
+namespace Yapoml.Extensions.Logging.Serilog
+{
+    public class SerilogAdapter : Yapoml.Logging.ILogger
+    {
+        private readonly ILogger _logger;
+
+        public SerilogAdapter(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Trace(string message)
+        {
+            _logger.Verbose(message);
+        }
+    }
+}
