8ed90029b6769dcf13a55f1120365fca4f62708a
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2022-05-20 17:15:33 +0300

    Use services container

--- src/Yapoml.Extensions.Logging.Serilog/SpaceOptionsExtension.cs
+++ src/Yapoml.Extensions.Logging.Serilog/SpaceOptionsExtension.cs
@@ -8,14 +8,14 @@ namespace Yapoml
     {
         public static ISpaceOptions UseSerilog(this ISpaceOptions spaceOptions)
         {
-            spaceOptions.Register<Framework.Logging.ILogger>(new SerilogAdapter(Log.Logger));
+            spaceOptions.Services.Register<Framework.Logging.ILogger>(new SerilogAdapter(Log.Logger));
 
             return spaceOptions;
         }
 
         public static ISpaceOptions UseSerilog(this ISpaceOptions spaceOptions, ILogger logger)
         {
-            spaceOptions.Register<Framework.Logging.ILogger>(new SerilogAdapter(logger));
+            spaceOptions.Services.Register<Framework.Logging.ILogger>(new SerilogAdapter(logger));
 
             return spaceOptions;
         }
